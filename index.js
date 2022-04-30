0a032af7a95486fabd1d0a628a1ddfe3b22b5f83
stefaniavvm <stefy_vali95@hotmail.com>
2022-04-30 15:07:46 +0200

    primer comentario

---
+++ index.js
@@ -0,0 +1,49 @@
+const myFavoriteHero = "Hulk";
+//const x = 50;
+const h = 5;
+// const y = 10;
+
+//const z = "h" + "y";
+
+//ejercicio 2
+
+let character = { name: "Jack Sparrow", age:10 };
+
+character = {age : 25};
+
+console.log(character); //Resultado: (name: "Jack Sparrow", age:25)
+
+//ejercicio 3
+
+const firstName = "Jon";
+const lastName ="Snow";
+const age = 24;
+
+console.log("Soy " + firstName + " "+ lastName + " " + "," + "tengo" + " " + age +" "+ "y me gustan todos los lobos.");
+// Resultado: Soy Jon Snow ,tengo 24 y me gustan todos los lobos
+
+//ejercicio 3: Operadores
+
+const num1 = 10;
+const num2 = 5;
+console.log(10 * 5); //Resultado 50
+
+const num3 = 10;
+const num4 = 2;
+console.log(10/2); //Resultado 5
+
+const num5 = 15;
+const num6 = 9;
+let total = num5 / num6
+console.log(total); //Resultado 1.6666666666666667
+
+//1.4
+
+const y = 10 , z=5 ;
+
+let x = y + z ;
+console.log(x);
+
+
+
+ 
